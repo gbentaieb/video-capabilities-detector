f34f9af87b7939e487c48a6881bed00aeb5ed4f3
Guillaume Bentaieb <guillaume.bentaieb@canal-plus.com>
2018-12-07 16:14:29 +0100

    feat(ui): add material ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
 import store from './ui/store';
 import Table from './ui/components/containers/Table';
+
 import './App.css';
+
+const styles = {
+  typography: {
+    margin: 'auto',
+  },
+}
 
 class App extends Component {
   render() {
+    const { classes: { typography } } = this.props;
     return (
       <Provider store={store}>
         <div className="App">
+          <AppBar position= 'sticky'>
+            <Toolbar>
+            <Typography variant="h6" color="inherit" classes={{root: typography}}>
+              Video Capabilities Detector
+            </Typography>
+            </Toolbar>
+          </AppBar>
           <Table />
         </div>
       </Provider>
@@ -16,4 +36,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
