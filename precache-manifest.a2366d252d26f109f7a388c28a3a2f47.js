df1089681a52941b7c284c9eadb6f565ae9c947a
Guillaume Bentaieb <guillaume.bentaieb@canal-plus.com>
2018-12-11 17:53:04 +0100

    Updates

--- precache-manifest.a2366d252d26f109f7a388c28a3a2f47.js
+++ precache-manifest.a2366d252d26f109f7a388c28a3a2f47.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/video-capacities-detector/static/js/runtime~main.80d5a0a4.js"
   },
   {
-    "revision": "e182253bed33a4fac348",
-    "url": "/video-capacities-detector/static/js/main.e182253b.chunk.js"
+    "revision": "67ad96a993d5932e44d1",
+    "url": "/video-capacities-detector/static/js/main.67ad96a9.chunk.js"
   },
   {
     "revision": "78fa599e9a90c93e4476",
     "url": "/video-capacities-detector/static/js/1.78fa599e.chunk.js"
   },
   {
-    "revision": "e182253bed33a4fac348",
+    "revision": "67ad96a993d5932e44d1",
     "url": "/video-capacities-detector/static/css/main.98f78969.chunk.css"
   },
   {
-    "revision": "1765acbc8fd56791ca55ffe6449f3b59",
+    "revision": "e964c52ac1ae94e3bd005f58533aaa84",
     "url": "/video-capacities-detector/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/video-capacities-detector/precache-manifest.36fb3a414b516a4f43e5e316ab04ded0.js"
+  "/video-capacities-detector/precache-manifest.a2366d252d26f109f7a388c28a3a2f47.js"
 );
 
 workbox.clientsClaim();
