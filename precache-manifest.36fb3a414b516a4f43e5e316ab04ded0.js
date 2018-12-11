26692afafcbd114fcb65fc41f625ca88548ef821
Guillaume Bentaieb <guillaume.bentaieb@canal-plus.com>
2018-12-11 17:37:48 +0100

    Updates

---
+++ precache-manifest.36fb3a414b516a4f43e5e316ab04ded0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80d5a0a491da928148fa",
+    "url": "/video-capacities-detector/static/js/runtime~main.80d5a0a4.js"
+  },
+  {
+    "revision": "e182253bed33a4fac348",
+    "url": "/video-capacities-detector/static/js/main.e182253b.chunk.js"
+  },
+  {
+    "revision": "78fa599e9a90c93e4476",
+    "url": "/video-capacities-detector/static/js/1.78fa599e.chunk.js"
+  },
+  {
+    "revision": "e182253bed33a4fac348",
+    "url": "/video-capacities-detector/static/css/main.98f78969.chunk.css"
+  },
+  {
+    "revision": "1765acbc8fd56791ca55ffe6449f3b59",
+    "url": "/video-capacities-detector/index.html"
+  }
+];
